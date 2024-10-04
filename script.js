ebb9f4af784154e385e4c650fab11e83dacd1929
Simyolo Sokhaba <124871610+Yolo-Designs@users.noreply.github.com>
2024-10-04 18:03:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -45,4 +45,15 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("Failed to send message. Please try again.");
         });
     });
+
+    // Toggle Menu (Hamburger Icon)
+    function toggleMenu() {
+        const menu = document.querySelector(".menu-links");
+        const icon = document.querySelector(".hamburger-icon");
+        menu.classList.toggle("open");
+        icon.classList.toggle("open");
+    }
+
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+    hamburgerIcon.addEventListener("click", toggleMenu);
 });
